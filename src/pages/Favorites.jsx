8ff6220c004f3cc703f122e855162cffc35d85ca
Casagrande <casagrandesoaresj@gmail.com>
2025-10-19 17:12:02 -0300

    working base project

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorite() {
+    return (
+        <div className="favorites-empty">
+            <h2>No Favorite Movies Yet</h2>
+            <p>Start adding movies to your favorites and they will appear</p>
+        </div>
+    )
+}
+
+export default Favorite
