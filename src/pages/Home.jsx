fc55a2425ef07e1b3081a26b7b02f5a68e2a69a1
Casagrande <casagrandesoaresj@gmail.com>
2025-10-23 00:43:23 -0300

    working. concluded state.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import MovieCard from "../components/MovieCard";
+import {useState} from "react"
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "Terminator", release_date: "1999" },
     { id: 3, title: "The Matrix", release_date: "1998" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault()
+    setSearchQuery("-------")
+  };
 
   return (
     <div className="home">
@@ -16,6 +22,8 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">
           Search
@@ -23,7 +31,8 @@ function Home() {
       </form>
 
       <div className="movies-grid">
-        {movies.map((movie) => (
+        {movies.map((movie) => 
+        movie.title.toLocaleLowerCase().startsWith(searchQuery) && (
           <MovieCard movie={movie} key={movie.id} />
         ))}
       </div>
@@ -31,4 +40,4 @@ function Home() {
   );
 }
 
-export default Home
+export default Home;
